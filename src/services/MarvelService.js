88b845d16bcf16bf13303049000c0b901fdf05ef
Mairbek <madiev.1995@list.ru>
2024-05-17 17:15:58 +0200

    Data transformation and a component with a random character

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,6 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-    _apiKey = '7031ef0fb19005819373cdd4f3da1da4';
+    _apiKey = 'apikey=c5d6fc8b83116d92ed468ce36bac6c62'
 
     getResource = async (url) => {
         let res = await fetch(url);
@@ -12,12 +12,24 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transfromCharacter);
     }
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transfromCharacter(res.data.results[0]);
+    }
+
+    _transfromCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[1].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 
